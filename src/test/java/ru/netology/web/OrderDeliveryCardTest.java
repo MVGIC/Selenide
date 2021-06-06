e7637158f6041f5d9428ee08e884ef9230ee5e93
MVGIC <ex3ct@mail.ru>
2021-06-06 13:07:41 +0300

    test modified

---
+++ src/test/java/ru/netology/web/OrderDeliveryCardTest.java
@@ -0,0 +1,25 @@
+package ru.netology.web;
+
+import org.junit.jupiter.api.Test;
+
+import java.time.Duration;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selectors.*;
+import static com.codeborne.selenide.Selenide.*;
+
+class OrderDeliveryCardTest {
+
+      @Test
+    void shouldOrderCard() {
+          open("http://localhost:9999");
+          $("[data-test-id=city input]").setValue("Красноярск");
+          $("[data-test-id=date input]").setValue("09.06.21");
+          $("[data-test-id=name input]").setValue("Иван Иванов");
+          $("[data-test-id=phone input").setValue("+78005553535");
+          $("[data-test-id=agreement]").click();
+          $$("button").find(exactText("Забронировать")).click();
+          $(withText("Встреча успешно забронирована")).shouldBe(visible, Duration.ofSeconds(10));
+      }
+}
+
