71db0a155e66d808102563a37430b6c925204480
MVGIC <ex3ct@mail.ru>
2021-06-14 01:52:31 +0300

    test modified #3

--- src/test/java/ru/netology/web/OrderDeliveryCardTest.java
+++ src/test/java/ru/netology/web/OrderDeliveryCardTest.java
@@ -1,25 +1,31 @@
 package ru.netology.web;
 
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
 
 import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
-import static com.codeborne.selenide.Condition.*;
-import static com.codeborne.selenide.Selectors.*;
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.*;
 
 class OrderDeliveryCardTest {
 
-      @Test
+    @Test
     void shouldOrderCard() {
-          open("http://localhost:9999");
-          $("[data-test-id=city] input").setValue("Красноярск");
-          $("[data-test-id=date] input").setValue("09.06.21");
-          $("[data-test-id=name] input").setValue("Иван Иванов");
-          $("[data-test-id=phone] input").setValue("+78005553535");
-          $("[data-test-id=agreement]").click();
-          $$("button").find(exactText("Забронировать")).click();
-          $(withText("Встреча успешно забронирована")).shouldHave(exist, Duration.ofSeconds(10));
-      }
+        open("http://localhost:9999");
+        $("[data-test-id=city] input").setValue("Красноярск");
+        $("[data-test-id=date] input").doubleClick().sendKeys(Keys.BACK_SPACE);
+        String planningDate = LocalDate.now().plusDays(4).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+        $("[data-test-id=date] input").setValue(planningDate);
+        $("[data-test-id=name] input").setValue("Иван Иванов");
+        $("[data-test-id=phone] input").setValue("+78005553535");
+        $("[data-test-id=agreement]").click();
+        $$("button").find(exactText("Забронировать")).click();
+        $("[data-test-id='notification'] .notification__content").shouldBe(visible, Duration.ofSeconds(11))
+                .shouldHave(exactText("Встреча успешно забронирована на " + planningDate));
+    }
 }
 
